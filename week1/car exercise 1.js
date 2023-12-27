d0238f69b17fef93bdd0529f1d43a356278eede4
ShadowFrost3167 <olivertwist3167@gmail.com>
2023-12-27 19:49:23 +0200

    testing

---
+++ week1/car exercise 1.js
@@ -0,0 +1,10 @@
+let age= prompt("How old are you?");
+if (age < 18){
+    console.log("Sorry, you are too young to drive this car. Powering off.");
+}
+else if(age>18){
+    console.log("Powering on enjoy the ride!");
+}
+else{
+    console.log("Congraduations on your first year of driving. Enjoy the ride!")
+}
