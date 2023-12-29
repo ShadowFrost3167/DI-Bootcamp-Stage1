847a61acfd574a7195d0f7a220adf569c89cfe59
ShadowFrost3167 <olivertwist3167@gmail.com>
2023-12-29 18:41:04 +0200

    uploadingHomeworkDay2Week3

---
+++ week3/day2/exercise6/exercise6.js
@@ -0,0 +1,76 @@
+// Exercise 6 : Guess The Answers #3
+
+
+// Instructions
+
+// For each expression, predict what you think the output will be in a comment (//) without first running the command.
+
+// Of course, explain each prediction.
+
+// Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+
+// What is the output of each of the expressions below?
+
+//===================================================
+//===================================================
+
+5 + "34"
+// Prediction: 534, it will output the numeral 5 in front of the string 34.
+// Actual: 534
+
+5 - "4"
+// Prediction: 1
+// Actual:1
+
+10 % 5
+// Prediction: .5 it will calculate 10 percent of 5
+// Actual: 0 doesn't put in the decimal just the whole number?
+
+5 % 10
+// Prediction: .5 it will calculate 5 percent of 10
+// Actual: 5 but i don't understand why.
+
+"Java" + "Script"
+// Prediction: JavaScript it will add the two strings together
+// Actual: JavaScript
+
+" " + " "
+// Prediction: two spaces so a blank 2 space spot.
+// Actual: '  '
+
+" " + 0
+// Prediction: NaN a blank space is not a number.
+// Actual: ' 0', i guess that when the string is empty that it calculates it as nothing so we get an empty space and the 0.
+
+
+
+
+//=============================================
+// initially when i did the true and false portion of this homework i didn't understand that js converts true and false to numbers after doing that i redid this portion. true - 1 and false -0.
+true + true
+// Prediction: 2 because 1+1=2
+// Actual: 2
+
+true + false
+// Prediction: 1 because 1+0=1
+// Actual:1
+
+false + true
+// Prediction: 1 because 0+1=1
+// Actual:1
+
+false - true 
+// Prediction: -1 because 0-1 = -1 
+// Actual: -1
+
+!true
+// Prediction: sets value to true
+// Actual: false, the ! means not.
+
+3 - 4
+// Prediction: -1
+// Actual: -1
+
+"Bob" - "bill"
+// Prediction: NaN not a number can't do numeral functions with strings
+// Actual: NaN
