3033f70872e7a356f0c8e4d8a74986b46d848035
ShadowFrost3167 <olivertwist3167@gmail.com>
2023-12-30 15:18:41 +0200

    day1week4

---
+++ week4/day1/exercise2/exercise2.js
@@ -0,0 +1,20 @@
+// Exercise 2
+// 1. Create a structured HTML file linked to a JS file
+
+
+
+// 2. Write a Javascript function that takes a parameter: myAge
+console.log(Ariel(29));
+
+function Ariel(myAge){
+    let mum = 2*myAge;
+    return mum;
+}
+console.log(Ariel(29));
+// 3. In the function, return the age of my mum (my mum is twice my age)
+
+// 4. Call the function
+
+// 5. In the global scope, console.log the result of the function
+
+//i don't know if i've done that or not will ask for a review with daniel on it, because this concept just isn't sticking. 
