a552b42d53aed3f44ec321632d9430e4c8833841
ShadowFrost3167 <olivertwist3167@gmail.com>
2024-06-08 06:36:34 +0300

    attemptfivemillion

--- nodeJsLearn/index.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello Relly-Kins!!");
-
-const myModuel = require('./module');
--- week13/Simchat Torah Game.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Simchat Torah Game" tilewidth="32" tileheight="32" tilecount="1440" columns="60">
- <image source="assets/Free_Version/Tilemap/Tilemap.png" width="1920" height="768"/>
-</tileset>
